9ca3a8f4ccb7f52ee6e9f8d5d4af121ba3a45a99
nanmu42 <i@nanmu.me>
2018-08-03 17:59:15 +0800

    [dev] helper type for better unmarshal

--- helper.go
+++ helper.go
@@ -9,7 +9,18 @@ package etherscan
 
 import (
 	"math/big"
+	"strconv"
+	"time"
 )
+
+// compose adds input to param, whose key is tag
+// if input is nil, compose is a no-op.
+func compose(param map[string]interface{}, tag string, input interface{}) {
+	if input == nil {
+		return
+	}
+	param[tag] = input
+}
 
 // M is a type shorthand for param input
 type M map[string]interface{}
@@ -28,4 +39,42 @@ func (b *BigInt) UnmarshalText(text []byte) (err error) {
 
 	*b = BigInt(*bigInt)
 	return nil
+}
+
+// MarshalText implements the encoding.TextMarshaler
+func (b *BigInt) MarshalText() (text []byte, err error) {
+	return []byte(b.Int().String()), nil
+}
+
+// Int returns b's *big.Int form
+func (b *BigInt) Int() *big.Int {
+	return (*big.Int)(b)
+}
+
+// Time is a wrapper over big.Int to implement only unmarshalText
+// for json decoding.
+type Time time.Time
+
+// UnmarshalText implements the encoding.TextUnmarshaler interface.
+func (t *Time) UnmarshalText(text []byte) (err error) {
+	input, err := strconv.ParseInt(string(text), 10, 64)
+	if err != nil {
+		err = wrapErr(err, "strconv.ParseInt")
+		return
+	}
+
+	var timestamp = time.Unix(input, 0)
+	*t = Time(timestamp)
+
+	return nil
+}
+
+// Time returns t's time.Time form
+func (t Time) Time() time.Time {
+	return time.Time(t)
+}
+
+// MarshalText implements the encoding.TextMarshaler
+func (t *Time) MarshalText() (text []byte, err error) {
+	return []byte(strconv.FormatInt(t.Time().Unix(), 10)), nil
 }
