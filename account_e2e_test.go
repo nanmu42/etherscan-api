a1906b301dc12fe2befba26f90b03fe131d3a4a6
nanmu42 <i@nanmu.me>
2018-08-03 18:00:46 +0800

    [dev] draft for e2e test
    The rate limit is not working as intended.

---
+++ account_e2e_test.go
@@ -0,0 +1,116 @@
+/*
+ * Copyright (c) 2018 LI Zhennan
+ *
+ * Use of this work is governed by a MIT License.
+ * You may find a license copy in project root.
+ */
+
+package etherscan
+
+import (
+	"encoding/json"
+	"fmt"
+	"math/big"
+	"testing"
+)
+
+func TestClient_AccountBalance(t *testing.T) {
+	balance, err := api.AccountBalance("0x281055afc982d96fab65b3a49cac8b878184cb16")
+	noError(t, err, "api.AccountBalance")
+
+	if balance.Int().Cmp(big.NewInt(0)) != 1 {
+		t.Fatalf("rich man is no longer rich")
+	}
+}
+
+func TestClient_MultiAccountBalance(t *testing.T) {
+	balances, err := api.MultiAccountBalance(
+		"0x281055afc982d96fab65b3a49cac8b878184cb16",
+		"0x6f46cf5569aefa1acc1009290c8e043747172d89",
+		"0x90e63c3d53e0ea496845b7a03ec7548b70014a91",
+		"0x53d284357ec70ce289d6d64134dfac8e511c8a3d")
+	noError(t, err, "api.MultiAccountBalance")
+
+	for i, item := range balances {
+		if item.Account == "" {
+			t.Errorf("bound error on index %v", i)
+		}
+		if item.Balance.Int().Cmp(big.NewInt(0)) != 1 {
+			t.Errorf("rich man %s at index %v is no longer rich.", item.Account, i)
+		}
+	}
+}
+
+func TestClient_NormalTxByAddress(t *testing.T) {
+	const wantLen = 19
+
+	var a, b = 54092, 79728
+	txs, err := api.NormalTxByAddress("0xde0b295669a9fd93d5f28d9ec85e40f4cb697bae", &a, &b, 1, 500, false)
+	noError(t, err, "api.NormalTxByAddress")
+
+	//j, _ := json.MarshalIndent(txs, "", "  ")
+	//fmt.Printf("%s\n", j)
+
+	if len(txs) != wantLen {
+		t.Errorf("got txs length %v, want %v", len(txs), wantLen)
+	}
+}
+
+func TestClient_InternalTxByAddress(t *testing.T) {
+	const wantLen = 66
+
+	var a, b = 0, 2702578
+	txs, err := api.InternalTxByAddress("0x2c1ba59d6f58433fb1eaee7d20b26ed83bda51a3", &a, &b, 1, 500, false)
+	noError(t, err, "api.InternalTxByAddress")
+
+	//j, _ := json.MarshalIndent(txs, "", "  ")
+	//fmt.Printf("%s\n", j)
+
+	if len(txs) != wantLen {
+		t.Errorf("got txs length %v, want %v", len(txs), wantLen)
+	}
+}
+
+func TestClient_ERC20TransferTx(t *testing.T) {
+	const wantLen = 3
+
+	var a, b = 3273004, 3328071
+	var contract, address = "0xe0b7927c4af23765cb51314a0e0521a9645f0e2a", "0x4e83362442b8d1bec281594cea3050c8eb01311c"
+	txs, err := api.ERC20TransferTx(&contract, &address, &a, &b, 1, 500)
+	noError(t, err, "api.ERC20TransferTx")
+
+	//j, _ := json.MarshalIndent(txs, "", "  ")
+	//fmt.Printf("%s\n", j)
+
+	if len(txs) != wantLen {
+		t.Errorf("got txs length %v, want %v", len(txs), wantLen)
+	}
+}
+
+func TestClient_BlocksMinedByAddress(t *testing.T) {
+	const wantLen = 10
+
+	blocks, err := api.BlocksMinedByAddress("0x9dd134d14d1e65f84b706d6f205cd5b1cd03a46b", 1, wantLen)
+	noError(t, err, "api.BlocksMinedByAddress")
+
+	//j, _ := json.MarshalIndent(blocks, "", "  ")
+	//fmt.Printf("%s\n", j)
+
+	if len(blocks) != wantLen {
+		t.Errorf("got txs length %v, want %v", len(blocks), wantLen)
+	}
+}
+
+func TestClient_UnclesMinedByAddress(t *testing.T) {
+	const wantLen = 10
+
+	blocks, err := api.UnclesMinedByAddress("0x9dd134d14d1e65f84b706d6f205cd5b1cd03a46b", 1, wantLen)
+	noError(t, err, "api.UnclesMinedByAddress")
+
+	j, _ := json.MarshalIndent(blocks, "", "  ")
+	fmt.Printf("%s\n", j)
+
+	if len(blocks) != wantLen {
+		t.Errorf("got txs length %v, want %v", len(blocks), wantLen)
+	}
+}
---
+++ setup_e2e_test.go
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) 2018 LI Zhennan
+ *
+ * Use of this work is governed by a MIT License.
+ * You may find a license copy in project root.
+ */
+
+package etherscan
+
+import (
+	"testing"
+	"time"
+)
+
+var (
+	// api test client for many test cases
+	api *Client
+	// bucket default rate limiter
+	bucket *Bucket
+)
+
+func init() {
+	bucket = NewBucket(5, 200*time.Microsecond)
+
+	api = New(Mainnet, "etherscan-api-e2e-test")
+	api.Verbose = true
+	api.BeforeRequest = func(module string, action string, param map[string]interface{}) error {
+		bucket.Take()
+		return nil
+	}
+}
+
+// Bucket is a simple and easy rate limiter
+// Use NewBucket() to construct one.
+type Bucket struct {
+	bucket     chan bool
+	refillTime time.Duration
+}
+
+// NewBucket factory of Bucket
+func NewBucket(cap int, refillTime time.Duration) (b *Bucket) {
+	b = &Bucket{
+		bucket:     make(chan bool, cap-1),
+		refillTime: refillTime,
+	}
+
+	go b.fillRoutine()
+
+	return
+}
+
+// Take a action token from bucket,
+// blocks if there is currently no token left.
+func (b *Bucket) Take() {
+	b.bucket <- true
+}
+
+// fill a action token into bucket,
+// blocks if the bucket is currently full
+func (b *Bucket) fill() {
+	<-b.bucket
+}
+
+func (b *Bucket) fillRoutine() {
+	ticker := time.NewTicker(b.refillTime)
+
+	for range ticker.C {
+		b.fill()
+	}
+}
+
+// noError checks for testing error
+func noError(t *testing.T, err error, msg string) {
+	if err != nil {
+		t.Fatalf("%s: %v", msg, err)
+	}
+}
