5cf064059ce9840ea7e1664139d44693fd318620
nanmu42 <i@nanmu.me>
2018-08-06 16:06:18 +0800

    [doc] enhance doc

--- doc.go
+++ doc.go
@@ -1,8 +1,8 @@
 // Package etherscan provides Go bindings to the Etherscan.io API.
 //
-// The implementation is with nearly Full implementation
+// This work is a nearly Full implementation
 // (accounts, transactions, tokens, contracts, blocks, stats),
-// full network support(Mainnet, Ropsten, Kovan, Rinkby, Tobalaba),
+// with full network support(Mainnet, Ropsten, Kovan, Rinkby, Tobalaba),
 // and only depending on standard library.
 //
 // Example can be found at https://github.com/nanmu42/etherscan-api
