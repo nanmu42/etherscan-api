dbb9f1b0a9930bc0d7986bac208dfbcce6e5eb12
LI Zhennan <i@nanmu.me>
2022-08-09 19:12:57 +0800

    fix CI test

--- logs_e2e_test.go
+++ logs_e2e_test.go
@@ -13,6 +13,7 @@ func TestClient_GetLogs(t *testing.T) {
 			Topics:          []string{"0xf63780e752c6a54a94fc52715dbc5518a3b4c3c2833d301a204226548a2a8545", "0x72657075746174696f6e00000000000000000000000000000000000000000000", "0x000000000000000000000000d9b2f59f3b5c7b3c67047d2f03c3e8052470be92"},
 			Data:            "0x",
 			BlockNumber:     "0x5c958",
+			BlockHash:       "0xe32a9cac27f823b18454e8d69437d2af41a1b81179c6af2601f1040a72ad444b",
 			TransactionHash: "0x0b03498648ae2da924f961dda00dc6bb0a8df15519262b7e012b7d67f4bb7e83",
 			LogIndex:        "0x",
 		},
